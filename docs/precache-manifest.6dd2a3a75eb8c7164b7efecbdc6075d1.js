666a8dd9c9ca45c75fb1c21bc559066a4f9b2581
jgimenoperez <jgimenoperez@gmail.com>
2021-07-21 09:00:16 +0200

    Ejecuto y subo buil

---
+++ docs/precache-manifest.6dd2a3a75eb8c7164b7efecbdc6075d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6baa22d16f8e5c38e2ca98c2fdac6cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a4bb25135927e09e705",
+    "url": "/static/js/2.1aeb0036.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1aeb0036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d00bfcc40a9fbae04d",
+    "url": "/static/js/main.b39441fe.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
